46cdfd91ba5151e9caa339276df1ee6973d4e92c
Devansh Garg <devanshgarg024@gmail.com>
2025-12-03 00:34:57 +0530

    Created a multiprocessing version

--- src/app/encryptDecrypt/Cryption.cpp
+++ src/app/encryptDecrypt/Cryption.cpp
@@ -1,6 +1,8 @@
 #include "Cryption.hpp"
 #include "../processes/Task.hpp"
 #include "../fileHandling/ReadEnv.cpp"
+#include <ctime>
+#include <iomanip>
 
 int executeCryption(const std::string& taskData) {
     Task task = Task::fromString(taskData);
@@ -10,21 +12,25 @@ int executeCryption(const std::string& taskData) {
     if (task.action == Action::ENCRYPT) {
         char ch;
         while (task.f_stream.get(ch)) {
-            std::streampos current_pos = task.f_stream.tellg();  
             ch = (ch + key) % 256;
-            task.f_stream.seekp(current_pos - (std::streamoff)1);
+            task.f_stream.seekp(-1, std::ios::cur);
             task.f_stream.put(ch);
         }
         task.f_stream.close();
     } else {
         char ch;
         while (task.f_stream.get(ch)) {
-            std::streampos current_pos = task.f_stream.tellg();
             ch = (ch - key + 256) % 256;
-            task.f_stream.seekp(current_pos - (std::streamoff)1);
+            task.f_stream.seekp(-1, std::ios::cur);
             task.f_stream.put(ch);
         }
         task.f_stream.close();
     }
+    std::time_t t = std::time(nullptr);
+    std::tm* now = std::localtime(&t);
+    std::string x = "encryption";
+    if(task.action == Action::DECRYPT)x = "decryption";
+    std::cout << "Exiting the "<<x<<" at: " << std::put_time(now, "%Y-%m-%d %H:%M:%S") << std::endl;
+    
     return 0;
 }
