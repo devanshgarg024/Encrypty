3482844a254b77e18fbf554798fdf0b123a67684
Devansh Garg <devanshgarg024@gmail.com>
2025-12-23 20:51:10 +0530

    minor changes

--- src/app/processes/ProcessManagement.cpp
+++ src/app/processes/ProcessManagement.cpp
@@ -16,7 +16,6 @@ void ProcessManagement::executeTasks() {
     while (!taskQueue.empty()) {
         std::unique_ptr<Task> taskToExecute = std::move(taskQueue.front());
         taskQueue.pop();
-        std::cout << "Executing task: " << taskToExecute->toString() << std::endl;
         executeCryption(taskToExecute->toString());
     }
 }
